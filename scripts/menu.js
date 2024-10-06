17f1d883b7b2b6c1c7d548f01b49cb04a2793fc3
Juan José Urquía <95894163+eljuanjo08@users.noreply.github.com>
2024-10-06 17:05:09 -0600

    first commit

---
+++ scripts/menu.js
@@ -0,0 +1,19 @@
+const button = document.getElementById('menu');
+const bars = document.getElementById('fa-bars');
+const xmark = document.getElementById('fa-xmark');
+const menuDisplay = document.getElementById('menu-display');
+let active = false;
+
+button.addEventListener('click', () => {
+    if (!active) {
+        bars.style.display = 'none';
+        xmark.style.display = 'block';
+        menuDisplay.style.display = 'flex';
+        active = true;
+    } else {
+        xmark.style.display = 'none';
+        bars.style.display = 'block';
+        menuDisplay.style.display = 'none';
+        active = false;
+    }
+});
